48f1814568587238a3cfb1928e1da0b847f6042f
Dylan Olivro <dylanolivro94@gmail.com>
2023-11-22 23:02:06 +0100

    fin page detail

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiConfigService } from '../api-config.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-details',
@@ -13,26 +14,38 @@ export class DetailsComponent implements OnInit {
   castingListCrew: any;
   castingListCast: any;
   typeMedia: string;
+  private routeSub: Subscription;
 
   constructor(
     private apiConfig: ApiConfigService,
-    public route: ActivatedRoute
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    const id: string = this.route.snapshot.paramMap.get('id');
-    const typeMedia: string = this.route.snapshot.paramMap.get('type');
-    this.typeMedia = typeMedia;
-    this.getDetailsAllInfos(id, typeMedia);
-    this.getCasting(id, typeMedia);
-    this.getSimilar(id, typeMedia);
+    this.routeSub = this.route.params.subscribe((params) => {
+      // Récupérez le nouveau paramètre d'identifiant et actualisez vos données ici
+      let id = params['id'];
+      let typeMedia = params['type'];
+      this.typeMedia = typeMedia;
+
+      // Mettez à jour vos données en fonction du nouvel identifiant
+      this.getDetailsAllInfos(id, typeMedia);
+      if (typeMedia === 'tv' || typeMedia === 'movie') {
+        this.getCasting(id, typeMedia);
+        this.getSimilar(id, typeMedia);
+      }
+    });
   }
+
+  // ngOnDestroy() {
+  //   this.routeSub.unsubscribe();
+  // }
 
   getDetailsAllInfos(id: string, typeMedia: string) {
     this.apiConfig
       .getDetailsFromApi(id, typeMedia)
       .subscribe((data) => (this.detailsAll = data));
-    // .subscribe((data) => console.log(data));
+      // .subscribe((data) => console.log(data));
   }
 
   getMediaInfo() {
@@ -45,13 +58,11 @@ export class DetailsComponent implements OnInit {
   }
 
   getCasting(id: string, typeMedia: string) {
-    this.apiConfig
-      .getCastingFromApi(id, typeMedia)
-      .subscribe((data) => {
-        this.castingListCast = data.cast;
-        this.castingListCrew = data.crew;
-      });
-      // .subscribe((data) => console.log(data.cast));
+    this.apiConfig.getCastingFromApi(id, typeMedia).subscribe((data) => {
+      this.castingListCast = data.cast;
+      this.castingListCrew = data.crew;
+    });
+    // .subscribe((data) => console.log(data.cast));
     // .subscribe((data) => console.log(data.cast));
   }
 
